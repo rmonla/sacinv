519679966ee21b39436458d9eb9913ebbae646e7
Ricardo N. Monla <rmonla@gmail.com>
2013-06-29 02:55:07 -0300

    corrección en archivo conectar.php

--- sacinv_v0.2/bd/conectar.php
+++ sacinv_v0.2/bd/conectar.php
@@ -3,17 +3,17 @@
     $mysql_host       = "localhost";
     $mysql_usuario    = "root";
     $mysql_contrasena = "";
-    $basedatos        = "sacinv_v_0_2";
+    $basedatos        = "bd_sacinv_v_0_2";
   /* Conecto al motor de base de datos */
     if (!($conexion_mysql = mysql_connect($mysql_host, $mysql_usuario,$mysql_contrasena))){
-       echo "no se pudo conectar";
+       echo "ERROR: <br>No se pudo establecer la conexión con la BD <em><b>".$basedatos.'</b></em>';
        exit;
     }
   /* Guardo la conexion en una variable global para las funciones*/
     $_SESSION['conexion'] = $conexion_mysql;
   /* Selecciono la base de datos */
     if (!mysql_select_db($basedatos, $conexion_mysql)){
-       echo "no se selecciono base da datos";
+       echo "ERROR: <br>No se pudo seleccionar la BD <em><b>".$basedatos.'</b></em>';
        exit;
     }
 ?>
